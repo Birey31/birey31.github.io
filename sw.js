47f122679c1d18ae901be50100e1f618cbb173b3
Birey31 <100028750+Birey31@users.noreply.github.com>
2026-01-05 17:33:11 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,48 @@
+const cacheName = "reeha-v3";
+const assets = [
+  "./",
+  "./index.html",
+  "./2026.html",
+  "./manifest.json",
+  "./media/mobillogo.webp",
+  "./media/logo.webp",
+  "./media/ask.webp",
+  "./media/ask1.webp",
+  "./media/ask2.webp",
+  "./media/ask3.webp",
+  "./media/ask4.webp",
+  "./media/ask5.webp",
+  "./media/ask6.webp",
+  "./media/ask7.webp",
+  "./media/askim.mp3",
+  "./media/askim2.mp3"
+];
+
+// Dosyaları telefona kaydet (Önbelleğe al)
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Eski önbellekleri temizle
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((k) => k !== cacheName).map((k) => caches.delete(k))
+      );
+    })
+  );
+});
+
+// Resimleri ve şarkıları hafızadan oku (Hızlandır)
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
+  );
+});
